407f5f6b8503354afc999b2cb176b0df715295de
Jacob Kline <jacobk513@gmail.com>
2024-12-06 12:31:26 -0500

    3.6.3

--- Kanbas/Users/routes.js
+++ Kanbas/Users/routes.js
@@ -7,6 +7,19 @@ export default function UserRoutes(app) {
   const createUser = async (req, res) => { };
   const deleteUser = async (req, res) => { };
   const findAllUsers = async (req, res) => {
+    const { role, name } = req.query;
+    if (role) {
+      const users = await dao.findUsersByRole(role);
+      res.json(users);
+      return;
+    }
+    if (name) {
+      const users = await dao.findUsersByPartialName(name);
+      res.json(users);
+      return;
+    }
+
+
     const users = await dao.findAllUsers();
     res.json(users);
   };
