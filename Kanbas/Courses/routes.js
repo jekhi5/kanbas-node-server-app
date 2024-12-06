ddb20dadf460f1cfe22c34414ecf80d9f55ce265
Jacob Kline <jacobk513@gmail.com>
2024-12-06 16:21:51 -0500

    finished through 5.3.4

--- Kanbas/Courses/routes.js
+++ Kanbas/Courses/routes.js
@@ -1,15 +1,20 @@
 import * as dao from './dao.js';
 import * as modulesDao from "../Modules/dao.js";
 import * as assignmentsDao from "../Assignments/dao.js";
+import * as enrollmentsDao from "../Enrollments/dao.js"
 
 export default function CourseRoutes(app) {
   app.get('/api/courses', async (req, res) => {
     const courses = await dao.findAllCourses();
     res.send(courses);
   });
-  
   app.post("/api/courses", async (req, res) => {
     const course = await dao.createCourse(req.body);
+    const currentUser = req.session["currentUser"];
+    if (currentUser) {
+      await enrollmentsDao.enrollUserInCourse(currentUser._id, course._id);
+    }
+
     res.json(course);
   });
 
